e7e059a08c019f8321dfba13ac640f3181da77eb
MartinSchubert04 <tinchoschubert04429@gmail.com>
2024-11-24 15:16:46 -0300

    agreado de animaciones, scroll bar y switch de categorias

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ function downloadCV() {
 
   link.click();
 }
+
+let lastScrollTop = 0;
+const sectionInputs = document.querySelector(".sectionInputs");
+
+// Detecta el evento de desplazamiento
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Si se hace scroll hacia abajo, oculta la barra
+  if (currentScroll > lastScrollTop) {
+    sectionInputs.classList.add("hidden");
+  } else {
+    sectionInputs.classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Previene un valor negativo
+});
+
+// Detecta cuando el mouse entra en la zona de la barra
+sectionInputs.addEventListener("mouseenter", function () {
+  sectionInputs.classList.remove("hidden");
+});
+
+// Detecta cuando el mouse sale de la zona de la barra
+sectionInputs.addEventListener("mouseleave", function () {
+  sectionInputs.classList.add("hidden");
+});
+
+document.querySelectorAll(".radio").forEach((radio) => {
+  radio.addEventListener("change", function () {
+    const targetId = button.getAttribute("data-target");
+    const targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
