90b5f849eb9b73c868bff04cc65566f67206d332
MartinSchubert04 <tinchoschubert04429@gmail.com>
2025-11-25 17:50:56 -0300

    bkp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css"
 import { AppRouter } from "./routes"
 import { ToastContainer } from "@components/Toast/ToastContainer"
-import * as Tooltip from "@radix-ui/react-tooltip"
 
 function App() {
   return (
     <div className="App">
-      <Tooltip.Provider delayDuration={100} skipDelayDuration={0}>
-        <AppRouter />
-        <ToastContainer />
-      </Tooltip.Provider>
+      <AppRouter />
+      <ToastContainer />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Portfolio/",
+  build: {
+    sourcemap: false,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
